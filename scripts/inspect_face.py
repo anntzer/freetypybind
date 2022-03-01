eabe947ecb77f68e49199f1ed666a50d282eeb06
Antony Lee <anntzer.lee@gmail.com>
2022-03-01 10:30:56 +0100

    Update inspect_face.

--- scripts/inspect_face.py
+++ scripts/inspect_face.py
@@ -1,14 +1,23 @@
 import os
 import pprint
+import subprocess
 
 import freetypybind as ft
 import freetypybind.query_face
 
 
+def _fc_paths():
+    return sorted(subprocess.run(["fc-list", "--format=%{file}\n"],
+                                 stdout=subprocess.PIPE).stdout.splitlines())
+
+
 def names(path: str, *, keys_only: bool = False):
-    face = ft.Face(os.fsencode(path), 0)
-    for face_index in range(face.num_faces):
-        face = ft.Face(os.fsencode(path), face_index)
+    if path == ":all:":
+        for path in _fc_paths():
+            names(path, keys_only=keys_only)
+        return
+    for face_index in range(ft.Face(path, 0).num_faces):
+        face = ft.Face(path, face_index)
         for key, value in face.get_sfnt_name_table().items():
             if keys_only:
                 print(*key)
@@ -17,9 +26,12 @@ def names(path: str, *, keys_only: bool = False):
 
 
 def query(path: str):
-    face = ft.Face(os.fsencode(path), 0)
-    for face_index in range(face.num_faces):
-        face = ft.Face(os.fsencode(path), face_index)
+    if path == ":all:":
+        for path in _fc_paths():
+            query(path)
+        return
+    for face_index in range(ft.Face(path, 0).num_faces):
+        face = ft.Face(path, face_index)
         pprint.pprint(ft.query_face.query_face(face))
 
 
